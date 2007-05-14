617e493c16f9a75f0a3841d09a995c09e4267e79
Trond A Ekseth <troeks@gmail.com>
2007-05-14 17:22:55 +0000

    oGlow - Don't enable inspect if Fizzle is enabled.

--- core/inspect.lua
+++ core/inspect.lua
@@ -29,9 +29,12 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ---------------------------------------------------------------------------]]
 
+if(select(4, GetAddOnInfo("Fizzle"))) then return end
+
 -- Globally used
 local G = getfenv(0)
 local select = select
+local pairs = pairs
 local createBorder = oGlow.createBorder
 
 local hook = CreateFrame"Frame"
