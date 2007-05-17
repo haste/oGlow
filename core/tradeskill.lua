4c9de9cb936bcf5b07edf15c2de7c16fff16ee22
Trond A Ekseth <troeks@gmail.com>
2007-05-17 21:02:23 +0000

    oGlow - Might want to include the tradeskill.lua file also.

---
+++ core/tradeskill.lua
@@ -0,0 +1,84 @@
+--[[-------------------------------------------------------------------------
+  Copyright (c) 2007, Trond A Ekseth
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above
+        copyright notice, this list of conditions and the following
+        disclaimer in the documentation and/or other materials provided
+        with the distribution.
+      * Neither the name of oGlow nor the names of its contributors
+        may be used to endorse or promote products derived from this
+        software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+---------------------------------------------------------------------------]]
+
+-- Globally used
+local G = getfenv(0)
+local select = select
+local oGlow = oGlow
+
+-- Tradeskill
+local GetTradeSkillNumReagents = GetTradeSkillNumReagents
+local GetTradeSkillReagentItemLink = GetTradeSkillReagentItemLink
+
+local GetItemInfo = GetItemInfo
+
+local update = function(id)
+	local icon = G["TradeSkillSkillIcon"]
+	local link = GetTradeSkillItemLink(id)
+
+	if(link) then
+		local q = select(3, GetItemInfo(link))
+		oGlow(icon, q)
+	elseif(icon.bc) then
+		icon.bc:Hide()
+	end
+
+	-- reagents
+	local reagents = GetTradeSkillNumReagents(id)
+	for i=1, reagents do
+		local frame = G["TradeSkillReagent"..i]
+		local link = GetTradeSkillReagentItemLink(id, i)
+
+		if(link) then
+			local q = select(3, GetItemInfo(link))
+			local point = G["TradeSkillReagent"..i.."IconTexture"]
+
+			oGlow(frame, q, point)
+		elseif(frame.bc) then
+			frame.bc:Hide()
+		end
+	end
+end
+
+if(IsAddOnLoaded("Blizzard_TradeSkillUI")) then
+	hooksecurefunc("TradeSkillFrame_SetSelection", update)
+else
+	local hook = CreateFrame"Frame"
+
+	hook:SetScript("OnEvent", function(self, event, addon)
+		if(addon == "Blizzard_TradeSkillUI") then
+			hooksecurefunc("TradeSkillFrame_SetSelection", update)
+			hook:UnregisterEvent"ADDON_LOADED"
+			hook:SetScript("OnEvent", nil)
+		end
+	end)
+	hook:RegisterEvent"ADDON_LOADED"
+end
