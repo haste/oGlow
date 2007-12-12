23dab13963be5d0d0f13ddaf0df1f44f9c1a7ae0
Trond A Ekseth <troeks@gmail.com>
2007-12-12 22:51:51 +0000

    oGlow: - Fixed buyback tab. - Bumped TOC to 1.4.

--- core/merchant.lua
+++ core/merchant.lua
@@ -43,17 +43,33 @@ local numPage = MERCHANT_ITEMS_PER_PAGE
 local MerchantFrame = MerchantFrame
 
 local update = function()
-	local numItems = GetMerchantNumItems()
-	for i=1, numPage do
-		local index = (((MerchantFrame.page - 1) * numPage) + i)
-		local link = GetMerchantItemLink(index)
-		local button = G["MerchantItem"..i.."ItemButton"]
+	if(MerchantFrame.selectedTab == 1) then
+		local numItems = GetMerchantNumItems()
+		for i=1, numPage do
+			local index = (((MerchantFrame.page - 1) * numPage) + i)
+			local link = GetMerchantItemLink(index)
+			local button = G["MerchantItem"..i.."ItemButton"]
 
-		if(link and not oGlow.preventMerchant) then
-			local q = select(3, GetItemInfo(link))
-			oGlow(button, q)
-		elseif(button.bc) then
-			button.bc:Hide()
+			if(link and not oGlow.preventMerchant) then
+				local q = select(3, GetItemInfo(link))
+				oGlow(button, q)
+			elseif(button.bc) then
+				button.bc:Hide()
+			end
+		end
+	else
+		local numItems = GetNumBuybackItems()
+		for i=1, numPage do
+			local index = (((MerchantFrame.page - 1) * numPage) + i)
+			local link = GetBuybackItemLink(index)
+			local button = G["MerchantItem"..i.."ItemButton"]
+
+			if(link and not oGlow.preventBuyback) then
+				local q = select(3, GetItemInfo(link))
+				oGlow(button, q)
+			elseif(button.bc) then
+				button.bc:Hide()
+			end
 		end
 	end
 end
