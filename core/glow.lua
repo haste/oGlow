1ada6af3ad2b25dddad2336ad96b519272627e9a
Trond A Ekseth <troeks@gmail.com>
2008-02-13 01:11:53 +0100

    Make the color table avaible to the rest of the world.

--- core/glow.lua
+++ core/glow.lua
@@ -1,33 +1,35 @@
 --[[-------------------------------------------------------------------------
-Copyright (c) 2007, Trond A Ekseth
-All rights reserved.
+  Copyright (c) 2007-2008, Trond A Ekseth
+  All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
 
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above
-copyright notice, this list of conditions and the following
-disclaimer in the documentation and/or other materials provided
-with the distribution.
-* Neither the name of oGlow nor the names of its contributors
-may be used to endorse or promote products derived from this
-software without specific prior written permission.
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above
+        copyright notice, this list of conditions and the following
+        disclaimer in the documentation and/or other materials provided
+        with the distribution.
+      * Neither the name of oGlow nor the names of its contributors may
+        be used to endorse or promote products derived from this
+        software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ---------------------------------------------------------------------------]]
+
+local type = type
 
 local colorTable = setmetatable({
 	[100] = {r = .9, g = 0, b = 0},
@@ -35,7 +37,7 @@ local colorTable = setmetatable({
 }, {__call = function(self, val)
 	local c = self[val]
 	if(c) then return c.r, c.g, c.b
-	else return GetItemQualityColor(val) end
+	elseif(type(val) == "number") then return GetItemQualityColor(val) end
 end})
 
 local createBorder = function(self, point)
@@ -53,9 +55,12 @@ end
 
 local border, r, g, b
 oGlow = setmetatable({
+	RegisterColor = function(self, key, r, g, b)
+		colorTable[key] = {r = r, g = g, b = b}
+	end,
 }, {
 	__call = function(self, frame, quality, point)
-		if(quality and quality > 1) then
+		if(type(quality) == "number" and quality > 1 or type(quality) == "string") then
 			if(not frame.bc) then createBorder(frame, point) end
 
 			border = frame.bc
@@ -67,5 +72,5 @@ oGlow = setmetatable({
 		elseif(frame.bc) then
 			frame.bc:Hide()
 		end
-	end
+	end,
 })
