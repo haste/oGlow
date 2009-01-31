4a896ae83375dada729974cff587323ff585dc3f
Trond A Ekseth <troeks@gmail.com>
2009-01-31 18:57:09 +0100

    Add a comment to the colorTable.

--- oglow.lua
+++ oglow.lua
@@ -3,6 +3,10 @@ local oGlow = CreateFrame('Frame', 'oGlow')
 local colorTable = setmetatable(
 	{},
 
+	-- We mainly want to handle item quality coloring, so this acts as a fallback.
+	-- The bonus of doing this is that we don't really have to make any updates to
+	-- the add-on if any new item colors are added. It also caches unlike the old
+	-- version.
 	{__index = function(self, val)
 		local r, g, b = GetItemQualityColor(val)
 		self[val] = {r, g, b}
@@ -23,6 +27,7 @@ local createBorder = function(self, point)
 	bc:SetPoint("CENTER", point or self)
 	self.oGlowBC = bc
 end
+
 
 function oGlow:RegisterColor(name, r, g, b)
 	colorTable[name] = {r, g, b}
