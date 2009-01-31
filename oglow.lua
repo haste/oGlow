657e7ba6d47ae66981192e6ac4a7fed95df71e1a
Trond A Ekseth <troeks@gmail.com>
2009-01-31 18:48:32 +0100

    Strip down the core.

--- oglow.lua
+++ oglow.lua
@@ -1,13 +1,13 @@
-local type = type
+local colorTable = setmetatable(
+	{},
 
-local colorTable = setmetatable({
-	[100] = {r = .9, g = 0, b = 0},
-	[99] = {r = 1, g = 1, b = 0},
-}, {__call = function(self, val)
-	local c = self[val]
-	if(c) then return c.r, c.g, c.b
-	elseif(type(val) == "number") then return GetItemQualityColor(val) end
-end})
+	{__index = function(self, val)
+		local r, g, b = GetItemQualityColor(val)
+		self[val] = {r, g, b}
+
+		return self[val]
+	end},
+)
 
 local createBorder = function(self, point)
 	local bc = self:CreateTexture(nil, "OVERLAY")
@@ -22,24 +22,10 @@ local createBorder = function(self, point)
 	self.bc = bc
 end
 
-local border, r, g, b
-oGlow = setmetatable({
+local oGlow = {
 	RegisterColor = function(self, key, r, g, b)
-		colorTable[key] = {r = r, g = g, b = b}
+		colorTable[key] = {r, g, b}
 	end,
-}, {
-	__call = function(self, frame, quality, point)
-		if(type(quality) == "number" and quality > 1 or type(quality) == "string") then
-			if(not frame.bc) then createBorder(frame, point) end
+}
 
-			border = frame.bc
-			if(border) then
-				r, g, b = colorTable(quality)
-				border:SetVertexColor(r, g, b)
-				border:Show()
-			end
-		elseif(frame.bc) then
-			frame.bc:Hide()
-		end
-	end,
-})
+_G.oGlow = oGlow
