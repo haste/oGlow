57e8bed965d77c8e98a1d89b0d856504913e4735
Trond A Ekseth <troeks@gmail.com>
2009-02-04 20:53:16 +0100

    Enable tradeskill pipe.

--- pipes/tradeskill.lua
+++ pipes/tradeskill.lua
@@ -1,57 +1,37 @@
--- Not update - so let's bail out early.
-do return end
-
--- Globally used
-local G = getfenv(0)
-local select = select
-local oGlow = oGlow
-
--- Tradeskill
-local GetTradeSkillNumReagents = GetTradeSkillNumReagents
-local GetTradeSkillReagentItemLink = GetTradeSkillReagentItemLink
-
-local GetItemInfo = GetItemInfo
-
-local icon, link, q, frame, point
 local update = function(id)
-	icon = G["TradeSkillSkillIcon"]
-	link = GetTradeSkillItemLink(id)
+	if(oGlow:IsPipeEnabled'tradeskill') then
+		local itemLink = GetTradeSkillItemLink(id)
 
-	if(link and not oGlow.preventTradeskill) then
-		q = select(3, GetItemInfo(link))
-		oGlow(icon, q)
-	elseif(icon.bc) then
-		icon.bc:Hide()
-	end
+		if(itemLink) then
+			oGlow:CallFilters('tradeskill', TradeSkillSkillIcon, itemLink)
+		end
 
-	for i=1, GetTradeSkillNumReagents(id) do
-		frame = G["TradeSkillReagent"..i]
-		link = GetTradeSkillReagentItemLink(id, i)
+		for i=1, GetTradeSkillNumReagents(id) do
+			local reagentFrame = _G['TradeSkillReagent'.. i]
+			local reagentLink = GetTradeSkillReagentItemLink(id, i)
 
-		if(link) then
-			q = select(3, GetItemInfo(link))
-			point = G["TradeSkillReagent"..i.."IconTexture"]
-
-			oGlow(frame, q, point)
-		elseif(frame.bc) then
-			frame.bc:Hide()
+			oGlow:CallFilters('tradeskill', reagentFrame, reagentLink)
 		end
 	end
 end
 
-if(IsAddOnLoaded("Blizzard_TradeSkillUI")) then
-	hooksecurefunc("TradeSkillFrame_SetSelection", update)
-else
-	local hook = CreateFrame"Frame"
-
-	hook:SetScript("OnEvent", function(self, event, addon)
-		if(addon == "Blizzard_TradeSkillUI") then
-			hooksecurefunc("TradeSkillFrame_SetSelection", update)
-			hook:UnregisterEvent"ADDON_LOADED"
-			hook:SetScript("OnEvent", nil)
-		end
-	end)
-	hook:RegisterEvent"ADDON_LOADED"
+local function ADDON_LOADED(self, event, addon)
+	if(addon == 'Blizzard_TradeSkillUI') then
+		hooksecurefunc('TradeSkillFrame_SetSelection', update)
+		self:UnregisterEvent(event, ADDON_LOADED)
+	end
 end
 
-oGlow.updateTradeskill = update
+local enable = function(self)
+	if(IsAddOnLoaded("Blizzard_TradeSkillUI")) then
+		hooksecurefunc("TradeSkillFrame_SetSelection", update)
+	else
+		self:RegisterEvent("ADDON_LOADED", ADDON_LOADED)
+	end
+end
+
+local disable = function(self)
+	self:UnregisterEvent('ADDON_LOADED', ADDON_LOADED)
+end
+
+oGlow:RegisterPipe('tradeskill', enable, disable, update)
