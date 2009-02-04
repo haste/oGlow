ca69963cef3ce6dfe271e8fafae7d98b4b5575c6
Trond A Ekseth <troeks@gmail.com>
2009-02-04 19:05:54 +0100

    Enable mail pipe.

--- pipes/mail.lua
+++ pipes/mail.lua
@@ -1,86 +1,66 @@
--- Not update - so let's bail out early.
-do return end
-
-local _G = getfenv(0)
-local oGlow = oGlow
-
-local select = select
-local ATTACHMENTS_MAX_SEND = ATTACHMENTS_MAX_SEND
-local GetSendMailItem = GetSendMailItem
+local stack = {}
 
 local send = function(self, event)
 	if(not SendMailFrame:IsShown()) then return end
 
 	for i=1, ATTACHMENTS_MAX_SEND do
-		local link = GetSendMailItemLink(i)
-		local slot = _G["SendMailAttachment"..i]
-		if(link and not oGlow.preventMail) then
-			local q = select(3, GetItemInfo(link))
-			oGlow(slot, q)
-		elseif(slot.bc) then
-			slot.bc:Hide()
-		end
+		local slotLink = GetSendMailItemLink(i)
+		local slotFrame = _G["SendMailAttachment"..i]
+		self:CallFilters('mail', slotFrame, slotLink)
 	end
 end
 
-local inbox = function(self, event)
-	local numItems = GetInboxNumItems()
-	local index = ((InboxFrame.pageNum - 1) * INBOXITEMS_TO_DISPLAY) + 1
+local inbox = function()
+	if(oGlow:IsPipeEnabled'mail') then
+		local numItems = GetInboxNumItems()
+		local index = ((InboxFrame.pageNum - 1) * INBOXITEMS_TO_DISPLAY) + 1
 
-	for i=1, INBOXITEMS_TO_DISPLAY do
-		local slot = _G["MailItem"..i.."Button"]
-		if (index <= numItems) then
-			local hq = 0
-			for j=1, ATTACHMENTS_MAX_RECEIVE do
-				local name = GetInboxItemLink(index, j)
-				if(name) then
-					-- I've always thought of (func()) to be completly useless, guess I was wrong
-					hq = math.max(hq, (select(3, GetItemInfo(name))))
+		for i=1, INBOXITEMS_TO_DISPLAY do
+			local slotFrame = _G["MailItem"..i.."Button"]
+			if (index <= numItems) then
+				wipe(stack)
+				for j=1, ATTACHMENTS_MAX_RECEIVE do
+					local attachLink = GetInboxItemLink(index, j)
+					if(attachLink) then
+						table.insert(stack, attachLink)
+					end
 				end
 			end
 
-			if(hq ~= 0 and not oGlow.preventMail) then
-				oGlow(slot, hq)
-			elseif(slot.bc) then
-				slot.bc:Hide()
-			end
-
-		elseif(slot.bc) then
-			slot.bc:Hide()
+			oGlow:CallFilters('mail', slotFrame, unpack(stack))
+			index = index + 1
 		end
-		index = index + 1
 	end
 end
 
-local addon = CreateFrame"Frame"
-addon:SetScript("OnEvent", function(self, event, ...)
-	self[event](self, event, ...)
-end)
+local letter = function(self)
+	if(oGlow:IsPipeEnabled'mail') then
+		if(not InboxFrame.openMailID) then return end
 
-hooksecurefunc("OpenMail_Update", function(self)
-	if(not InboxFrame.openMailID) then return end
+		for i=1, ATTACHMENTS_MAX_RECEIVE do
+			local itemLink = GetInboxItemLink(InboxFrame.openMailID, i)
+			if(itemLink) then
+				local slotFrame = _G["OpenMailAttachmentButton"..i]
 
-	for i=1, ATTACHMENTS_MAX_RECEIVE do
-		local name = GetInboxItemLink(InboxFrame.openMailID, i)
-		if(name) then
-			local slot = _G["OpenMailAttachmentButton"..i]
-			if(not oGlow.preventMail) then
-				oGlow(slot, select(3, GetItemInfo(name)))
-			elseif(slot.bc) then
-				slot.bc:Hide()
+				oGlow:CallFilters('mail', itemLink)
 			end
 		end
 	end
-end)
+end
 
-hooksecurefunc("InboxFrame_Update", inbox)
+local enable = function(self)
+	self:RegisterEvent('MAIL_SHOW', send)
+	self:RegisterEvent('MAIL_SEND_INFO_UPDATE', send)
+	self:RegisterEvent('MAIL_SEND_SUCCESS', send)
 
-addon.MAIL_SHOW = send
-addon.MAIL_SEND_INFO_UPDATE = send
-addon.MAIL_SEND_SUCCESS = send
+	hooksecurefunc("OpenMail_Update", letter)
+	hooksecurefunc("InboxFrame_Update", inbox)
+end
 
-addon:RegisterEvent"MAIL_SHOW"
-addon:RegisterEvent"MAIL_SEND_INFO_UPDATE"
-addon:RegisterEvent"MAIL_SEND_SUCCESS"
+local disable = function(self)
+	self:UnregisterEvent('MAIL_SHOW', send)
+	self:UnregisterEvent('MAIL_SEND_INFO_UPDATE', send)
+	self:UnregisterEvent('MAIL_SEND_SUCCESS', send)
+end
 
-oGlow.updateMail = update
+oGlow:RegisterPipe('mail', enable, disable, update)
