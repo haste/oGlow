b8a919c66cf23a6333af2a026359774abbc8099e
Trond A Ekseth <troeks@gmail.com>
2009-02-04 19:08:40 +0100

    Recursive filters - hoo~.

--- filters/quality.lua
+++ filters/quality.lua
@@ -1,10 +1,17 @@
-local quality = function(itemLink)
-	if(itemLink) then
-		local _, _, quality = GetItemInfo(itemLink)
+local quality = function(...)
+	local quality
 
-		if(quality > 1) then
-			return quality
+	for i=1, select('#', ...) do
+		local itemLink = select(i, ...)
+
+		if(itemLink) then
+			local _, _, itemQuality= GetItemInfo(itemLink)
+			quality = math.max(quality, itemQuality)
 		end
+	end
+
+	if(quality > 1) then
+		return quality
 	end
 end
 
