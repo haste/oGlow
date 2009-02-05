7e696eed39ef8fc0ada58e98e32840aa4c8574cc
Trond A Ekseth <troeks@gmail.com>
2009-02-05 10:55:15 +0100

    Enable the guild bank pipe.

--- pipes/gbank.lua
+++ pipes/gbank.lua
@@ -1,57 +1,25 @@
--- Not update - so let's bail out early.
-do return end
-
-local G = getfenv(0)
-local select = select
-local oGlow = oGlow
-
-local update = function()
+local update = function(self)
 	local tab = GetCurrentGuildBankTab()
-	local index, column, q
 	for i=1, MAX_GUILDBANK_SLOTS_PER_TAB do
-		index = math.fmod(i, NUM_SLOTS_PER_GUILDBANK_GROUP)
+		local index = math.fmod(i, NUM_SLOTS_PER_GUILDBANK_GROUP)
 		if(index == 0) then
 			index = NUM_SLOTS_PER_GUILDBANK_GROUP
 		end
-		column = ceil((i-0.5)/NUM_SLOTS_PER_GUILDBANK_GROUP)
+		local column = math.ceil((i-0.5)/NUM_SLOTS_PER_GUILDBANK_GROUP)
 
-		local link = GetGuildBankItemLink(tab, i)
-		local slot = _G["GuildBankColumn"..column.."Button"..index]
-		if(link and not oGlow.preventGBank) then
-			q = select(3, GetItemInfo(link))
-			oGlow(slot, q)
-		elseif(slot.bc) then
-			slot.bc:Hide()
-		end
+		local slotLink = GetGuildBankItemLink(tab, i)
+		local slotFrame = _G["GuildBankColumn"..column.."Button"..index]
+
+		self:CallFilters('gbank', slotFrame, slotLink)
 	end
 end
 
-local event = CreateFrame"Frame"
-event:SetScript("OnEvent", function(self, event, ...)
-	if(event == "GUILDBANKFRAME_OPENED") then
-		self:RegisterEvent"GUILDBANKBAGSLOTS_CHANGED"
-		self:Show()
-	elseif(event == "GUILDBANKBAGSLOTS_CHANGED") then
-		self:Show()
-	elseif(event == "GUILDBANKFRAME_CLOSED") then
-		self:UnregisterEvent"GUILDBANKBAGSLOTS_CHANGED"
-		self:Hide()
-	end
-end)
+local enable = function(self)
+	self:RegisterEvent('GUILDBANKBAGSLOTS_CHANGED', update)
+	self:RegisterEvent('GUILDBANKFRAME_OPENED', update)
+end
 
-local delay = 0
-event:SetScript("OnUpdate", function(self, elapsed)
-	delay = delay + elapsed
-	if(delay > .05) then
-		update()
-	
-		delay = 0
-		self:Hide()
-	end
-end)
-
-event:RegisterEvent"GUILDBANKFRAME_OPENED"
-event:RegisterEvent"GUILDBANKFRAME_CLOSED"
-event:Hide()
-
-oGlow.updateGBank = update
+local disable = function(self)
+	self:UnregisterEvent('GUILDBANKBAGSLOTS_CHANGED', update)
+	self:UnregisterEvent('GUILDBANKFRAME_OPENED', update)
+end
