a70e2c5788df4c7e1f247e82dd1404c084eddd06
Trond A Ekseth <troeks@gmail.com>
2009-02-05 15:54:43 +0100

    Make the merchant pipe update correctly.

--- pipes/merchant.lua
+++ pipes/merchant.lua
@@ -1,32 +1,31 @@
-local update = function(self, event)
-	if(MerchantFrame:IsShown()) then
+local hook
+
+local update = function()
+	if(MerchantFrame:IsShown() and oGlow:IsPipeEnabled'merchant') then
 		if(MerchantFrame.selectedTab == 1) then
 			for i=1, MERCHANT_ITEMS_PER_PAGE do
 				local index = (((MerchantFrame.page - 1) * MERCHANT_ITEMS_PER_PAGE) + i)
 				local itemLink = GetMerchantItemLink(index)
 				local slotFrame = _G['MerchantItem' .. i .. 'ItemButton']
 
-				self:CallFilters('merchant', slotFrame, itemLink)
+				oGlow:CallFilters('merchant', slotFrame, itemLink)
 			end
 		else
 			for i=1, BUYBACK_ITEMS_PER_PAGE do
 				local itemLink = GetBuybackItemInfo(i)
 				local slotFrame = _G['MerchantItem' .. i .. 'ItemButton']
 
-				self:CallFilters('merchant', slotFrame, itemLink)
+				oGlow:CallFilters('merchant', slotFrame, itemLink)
 			end
 		end
 	end
 end
 
 local enable = function(self)
-	self:RegisterEvent('MERCHANT_UPDATE', update)
-	self:RegisterEvent('MERCHANT_SHOW', update)
+	if(not hook) then
+		hooksecurefunc('MerchantFrame_Update', update)
+		hook = true
+	end
 end
 
-local disable = function(self)
-	self:UnregisterEvent('MERCHANT_UPDATE', update)
-	self:UnregisterEvent('MERCHANT_SHOW', update)
-end
-
-oGlow:RegisterPipe('merchant', enable, disable, update)
+oGlow:RegisterPipe('merchant', enable, nil, update)
