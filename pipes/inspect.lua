eb44ccaf36f381df293bb7be66f6242450c4578b
Trond A Ekseth <troeks@gmail.com>
2009-02-05 11:09:39 +0100

    Add a update hook to the inspect pipe.

--- pipes/inspect.lua
+++ pipes/inspect.lua
@@ -2,6 +2,7 @@
 
 if(select(4, GetAddOnInfo("Fizzle"))) then return end
 
+local hook
 local slots = {
 	"Head",
 	"Neck",
@@ -25,14 +26,14 @@ local slots = {
 }
 
 local update = function(self)
-	if(not InspectFrame:IsShown()) then return end
+	if(not InspectFrame:IsShown() or not oGlow:IsPipeEnabled'inspect') then return end
 
 	local unit = InspectFrame.unit
 	for i, slotName in next, slots do
 		local itemLink = GetInventoryItemLink(unit, i)
 		local inspectSlot = _G["Inspect"..slotName.."Slot"]
 
-		self:CallFilters('inspect', inspectSlot, itemLink)
+		oGlow:CallFilters('inspect', inspectSlot, itemLink)
 	end
 end
 
@@ -46,6 +47,11 @@ local function ADDON_LOADED(self, event, addon)
 	if(addon == "Blizzard_InspectUI") then
 		self:RegisterEvent("PLAYER_TARGET_CHANGED", update)
 		self:RegisterEvent('UNIT_INVENTORY_CHANGED', UNIT_INVENTORY_CHANGED)
+
+		if(not hook) then
+			InspectFrame:HookScript('OnShow', update)
+		end
+
 		self:UnregisterEvent("ADDON_LOADED", ADDON_LOADED)
 	end
 end
@@ -54,6 +60,10 @@ local enable = function(self)
 	if(IsAddOnLoaded("Blizzard_InspectUI")) then
 		self:RegisterEvent('PLAYER_TARGET_CHANGED', update)
 		self:RegisterEvent('UNIT_INVENTORY_CHANGED', UNIT_INVENTORY_CHANGED)
+
+		if(not hook) then
+			InspectFrame:HookScript('OnShow', update)
+		end
 	else
 		self:RegisterEvent("ADDON_LOADED", ADDON_LOADED)
 	end
