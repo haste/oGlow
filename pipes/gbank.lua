20da29102a92caec46dbdb3b54f59a660058ee8a
Trond A Ekseth <troeks@gmail.com>
2009-02-14 16:48:25 +0100

    Properly fix the gbank.

--- pipes/gbank.lua
+++ pipes/gbank.lua
@@ -2,7 +2,7 @@ local update = function(self)
 	-- We shouldn't really do this. The correct solution would be to delay the
 	-- event registration until Blizzard_GuildBankUI is loaded, but we use this
 	-- solution for now.
-	if(GuildBankFrame and not GuildBankFrame:IsShown()) then return end
+	if(not IsAddOnLoaded"Blizzard_GuildBankUI") then return end
 
 	local tab = GetCurrentGuildBankTab()
 	for i=1, 98 do
