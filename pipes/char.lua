c2a78bbaf7071cea7f8ce84fa5585233e36f94da
Trond A Ekseth <troeks@gmail.com>
2009-06-06 19:31:54 +0200

    Add a description to the character pipe.

--- pipes/char.lua
+++ pipes/char.lua
@@ -56,4 +56,4 @@ local disable = function(self)
 	self:UnregisterEvent('UNIT_INVENTORY_CHANGED', UNIT_INVENTORY_CHANGED)
 end
 
-oGlow:RegisterPipe('char', enable, disable, update)
+oGlow:RegisterPipe('char', enable, disable, update, [[The Blizzard character frame.]])
