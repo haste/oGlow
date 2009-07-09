c0fcee68f86277386bbbc6aa2ba7a5f4dd7823ff
Trond A Ekseth <troeks@gmail.com>
2009-07-10 01:51:06 +0200

    Scan the tooltips for information regarding quest items.
    The reason for this is that we can't really trust the item category blizzard assigns.

--- filters/quest.lua
+++ filters/quest.lua
@@ -1,10 +1,26 @@
+local questString = ITEM_BIND_QUEST
+
+local tip = _G.oGlowTip
+if(not tip) then
+	tip = CreateFrame('GameTooltip', 'oGlowTip')
+	tip:SetOwner(WorldFrame, 'ANCHOR_NONE')
+	tip.r, tip.l = {}, {}
+
+	for i=1,3 do
+		tip.l[i], tip.r[i] = tip:CreateFontString(nil, nil, "GameFontNormal"), tip:CreateFontString(nil, nil, "GameFontNormal")
+		tip:AddFontStrings(tip.l[i], tip.r[i])
+	end
+end
+
 local quest = function(...)
 	for i=1, select('#', ...) do
 		local itemLink = select(i, ...)
 
 		if(itemLink) then
-			local _, _, _, _, _, type = GetItemInfo(itemLink)
-			if(type == 'Quest') then
+			tip:ClearLines()
+			tip:SetHyperlink(itemLink)
+
+			if(tip:NumLines() > 1 and tip.l[2]:GetText() == questString) then
 				return 'Quest'
 			end
 		end
