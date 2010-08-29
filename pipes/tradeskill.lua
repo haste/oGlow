aa0de036d126ccd8c3e2d553f90a8fe9d93f1bed
Trond A Ekseth <troeks@gmail.com>
2010-08-29 15:32:55 +0200

    The tradeskill pipe will now correctly clear all displays when disabled.

--- pipes/tradeskill.lua
+++ pipes/tradeskill.lua
@@ -1,6 +1,9 @@
+-- TODO:
+--  - Write a description.
+
 local hook
 
-local update = function(id)
+local pipe = function(id)
 	if(oGlow:IsPipeEnabled'tradeskill') then
 		local itemLink = GetTradeSkillItemLink(id)
 
@@ -20,17 +23,24 @@ end
 local function ADDON_LOADED(self, event, addon)
 	if(addon == 'Blizzard_TradeSkillUI') then
 		if(not hook) then
-			hooksecurefunc('TradeSkillFrame_SetSelection', update)
+			hooksecurefunc('TradeSkillFrame_SetSelection', pipe)
 			hook = true
 		end
 		self:UnregisterEvent(event, ADDON_LOADED)
 	end
 end
 
+local update = function(self)
+	local id = GetTradeSkillSelectionIndex()
+	if(id and IsAddOnLoaded('Blizzard_TradeSkillUI')) then
+		return pipe(id)
+	end
+end
+
 local enable = function(self)
 	if(IsAddOnLoaded("Blizzard_TradeSkillUI")) then
 		if(not hook) then
-			hooksecurefunc("TradeSkillFrame_SetSelection", update)
+			hooksecurefunc("TradeSkillFrame_SetSelection", pipe)
 			hook = true
 		end
 	else
@@ -40,6 +50,13 @@ end
 
 local disable = function(self)
 	self:UnregisterEvent('ADDON_LOADED', ADDON_LOADED)
+
+	if(IsAddOnLoaded('Blizzard_TradeSkillUI')) then
+		for i=1, MAX_TRADE_SKILL_REAGENTS or 8 do
+			self:CallFilters('tradeskill', _G['TradeSkillReagent' .. i .. 'IconTexture'])
+		end
+		self:CallFilters('tradeskill', TradeSkillSkillIcon)
+	end
 end
 
 oGlow:RegisterPipe('tradeskill', enable, disable, update, 'Blizzard Tradeskill Frame', nil)
