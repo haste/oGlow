cc3ae084e1833c8af90c3ac3073309922ba77b49
Trond A Ekseth <troeks@gmail.com>
2011-10-30 10:48:03 +0100

    ui: Move Hex into helpers.

--- ui/helpers.lua
+++ ui/helpers.lua
@@ -7,3 +7,15 @@ ns.createFontString = function(parent, template)
 
 	return label
 end
+
+ns.Hex = function(r, g, b)
+	if(type(r) == "table") then
+		if(r.r) then
+			r, g, b = r.r, r.g, r.b
+		else
+			r, g, b = unpack(r)
+		end
+	end
+
+	return string.format("|cff%02x%02x%02x", r*255, g*255, b*255)
+end
--- ui/quality.lua
+++ ui/quality.lua
@@ -8,18 +8,6 @@ frame:Hide()
 frame.name = 'Quality'
 frame.parent = 'oGlow'
 
-local Hex = function(r, g, b)
-	if(type(r) == "table") then
-		if(r.r) then
-			r, g, b = r.r, r.g, r.b
-		else
-			r, g, b = unpack(r)
-		end
-	end
-
-	return string.format("|cff%02x%02x%02x", r*255, g*255, b*255)
-end
-
 frame:SetScript('OnShow', function(self)
 	self:CreateOptions()
 	self:SetScript('OnShow', nil)
@@ -72,7 +60,7 @@ function frame:CreateOptions()
 
 			for i=0,7 do
 				info = UIDropDownMenu_CreateInfo()
-				info.text = Hex(colorTable[i]) .._G['ITEM_QUALITY' .. i .. '_DESC']
+				info.text = ns.Hex(colorTable[i]) .._G['ITEM_QUALITY' .. i .. '_DESC']
 				info.value = i
 				info.func = DropDown_OnClick
 
