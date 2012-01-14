75edf84f20cf055a51f7324fce4de60cca4f44a0
Trond A Ekseth <troeks@gmail.com>
2012-01-14 20:34:29 +0100

    mail: Remove the duplicate update code in disable.

--- pipes/mail.lua
+++ pipes/mail.lua
@@ -1,7 +1,7 @@
 -- TODO:
---  - Possibly clean-up the dupe code.
 --  - Write a description.
 
+local _E
 local hook
 local stack = {}
 
@@ -11,45 +11,41 @@ local send = function(self)
 	for i=1, ATTACHMENTS_MAX_SEND do
 		local slotLink = GetSendMailItemLink(i)
 		local slotFrame = _G["SendMailAttachment"..i]
-		self:CallFilters('mail', slotFrame, slotLink)
+		self:CallFilters('mail', slotFrame, _E and slotLink)
 	end
 end
 
 local inbox = function()
-	if(oGlow:IsPipeEnabled'mail') then
-		local numItems = GetInboxNumItems()
-		local index = ((InboxFrame.pageNum - 1) * INBOXITEMS_TO_DISPLAY) + 1
+	local numItems = GetInboxNumItems()
+	local index = ((InboxFrame.pageNum - 1) * INBOXITEMS_TO_DISPLAY) + 1
 
-		for i=1, INBOXITEMS_TO_DISPLAY do
-			local slotFrame = _G["MailItem"..i.."Button"]
-			if (index <= numItems) then
-				for j=1, ATTACHMENTS_MAX_RECEIVE do
-					local attachLink = GetInboxItemLink(index, j)
-					if(attachLink) then
-						table.insert(stack, attachLink)
-					end
+	for i=1, INBOXITEMS_TO_DISPLAY do
+		local slotFrame = _G["MailItem"..i.."Button"]
+		if (index <= numItems) then
+			for j=1, ATTACHMENTS_MAX_RECEIVE do
+				local attachLink = GetInboxItemLink(index, j)
+				if(attachLink) then
+					table.insert(stack, attachLink)
 				end
 			end
-
-			oGlow:CallFilters('mail', slotFrame, unpack(stack))
-			wipe(stack)
-
-			index = index + 1
 		end
+
+		oGlow:CallFilters('mail', slotFrame, _E and unpack(stack))
+		wipe(stack)
+
+		index = index + 1
 	end
 end
 
 local letter = function()
-	if(oGlow:IsPipeEnabled'mail') then
-		if(not InboxFrame.openMailID) then return end
+	if(not InboxFrame.openMailID) then return end
 
-		for i=1, ATTACHMENTS_MAX_RECEIVE do
-			local itemLink = GetInboxItemLink(InboxFrame.openMailID, i)
-			if(itemLink) then
-				local slotFrame = _G["OpenMailAttachmentButton"..i]
+	for i=1, ATTACHMENTS_MAX_RECEIVE do
+		local itemLink = GetInboxItemLink(InboxFrame.openMailID, i)
+		if(itemLink) then
+			local slotFrame = _G["OpenMailAttachmentButton"..i]
 
-				oGlow:CallFilters('mail', slotFrame, itemLink)
-			end
+			oGlow:CallFilters('mail', slotFrame, _E and itemLink)
 		end
 	end
 end
@@ -60,30 +56,34 @@ local update = function(self)
 	letter()
 end
 
+local hookLetter = function(...)
+	if(_E) then return letter(...) end
+end
+
+local hookInbox = function(...)
+	if(_E) then return inbox(...) end
+end
+
 local enable = function(self)
+	_E = true
+
 	self:RegisterEvent('MAIL_SHOW', send)
 	self:RegisterEvent('MAIL_SEND_INFO_UPDATE', send)
 	self:RegisterEvent('MAIL_SEND_SUCCESS', send)
 
 	if(not hook) then
-		hooksecurefunc("OpenMail_Update", letter)
-		hooksecurefunc("InboxFrame_Update", inbox)
+		hooksecurefunc("OpenMail_Update", hookLetter)
+		hooksecurefunc("InboxFrame_Update", hookInbox)
 		hook = true
 	end
 end
 
 local disable = function(self)
+	_E =  nil
+
 	self:UnregisterEvent('MAIL_SHOW', send)
 	self:UnregisterEvent('MAIL_SEND_INFO_UPDATE', send)
 	self:UnregisterEvent('MAIL_SEND_SUCCESS', send)
-
-	for i=1, INBOXITEMS_TO_DISPLAY do
-		oGlow:CallFilters('mail', _G["MailItem"..i.."Button"])
-	end
-
-	for i=1, ATTACHMENTS_MAX_RECEIVE do
-		oGlow:CallFilters('mail', _G["OpenMailAttachmentButton"..i])
-	end
 end
 
 oGlow:RegisterPipe('mail', enable, disable, update, 'Mail frame', nil)
